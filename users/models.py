ccd962bcd04dcd5d3bd64d81c28def5bc9cb3983
Sehyuk Ahn <shyukahn@gmail.com>
2024-07-19 21:01:52 +0900

    feat: edit user room_id to foreign key

--- users/models.py
+++ users/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+from rooms.models import Room
+
 # Create your models here.
 class User(models.Model):
     google_account = models.CharField(max_length=100, null=False, primary_key=True, blank=False)
     nickname = models.CharField(max_length=100, null=False, blank=False)
     profile_image_url = models.URLField(null=False, blank=False)
-    current_room_id = models.IntegerField(null=True, default=None)
+    room_id = models.ForeignKey(Room, on_delete=models.SET_NULL, null=True)
     
     def __str__(self):
         return self.nickname
