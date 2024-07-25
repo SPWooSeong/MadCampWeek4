96507f0f08b7c236ff3b12298125deab0f291517
Sehyuk Ahn <shyukahn@gmail.com>
2024-07-25 15:35:24 +0900

    chore: asgi settings

--- OurPick/asgi.py
+++ OurPick/asgi.py
@@ -10,14 +10,16 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OurPick.settings')
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import rooms.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OurPick.settings')
-
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
+    'http': django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(
             rooms.routing.websocket_urlpatterns
